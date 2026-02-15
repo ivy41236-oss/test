7b6e6f594be1cc0004ab4ee0e2e4085afd8ad5a1
JP <jungil.park@hotmail.com>
2026-02-15 15:03:41 +0100

    Task1

--- java-assignment/src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
+++ java-assignment/src/test/java/com/fulfilment/application/monolith/location/LocationGatewayTest.java
@@ -1,18 +1,33 @@
 package com.fulfilment.application.monolith.location;
 
+import com.fulfilment.application.monolith.warehouses.domain.models.Location;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class LocationGatewayTest {
 
   @Test
   public void testWhenResolveExistingLocationShouldReturn() {
     // given
-    // LocationGateway locationGateway = new LocationGateway();
+    LocationGateway locationGateway = new LocationGateway();
 
     // when
-    // Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
+    Location location = locationGateway.resolveByIdentifier("ZWOLLE-001");
 
     // then
-    // assertEquals(location.identification, "ZWOLLE-001");
+    assertEquals(location.identification, "ZWOLLE-001");
+  }
+
+  @Test
+  public void testWhenResolveNonExistingLocationShouldReturnNull() {
+    // given
+    LocationGateway locationGateway = new LocationGateway();
+
+    // when
+    Location location = locationGateway.resolveByIdentifier("NONEXISTENT");
+
+    // then
+    assertNull(location);
   }
 }
